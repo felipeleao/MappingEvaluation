51e7ddd9f89dd20d7e3a2a499f214c316b506649
felipeleao <felipe@fleao.com.br>
2014-05-31 23:15:05 -0300

    Finished layout for main page
    — Also converted the pages to PHP

--- index.php
+++ index.php
@@ -17,9 +17,6 @@
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -39,15 +36,16 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Mapping Evaluation</a>
+          <a class="navbar-brand" href="#"><img src="images/LogoSmall.png">&nbsp;&nbsp;Mapping Evaluation</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li><a href="http://www.fleao.com.br">by Felipe Leão</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
+          <li><a href="../navbar/">Instructions</a></li>
             <li class="active"><a href="./">Evaluate!</a></li>
-            <li><a href="../navbar/">Instructions</a></li>
+              <li><a href="../navbar/">About the Research</a></li>
             <li><a href="../navbar-fixed-top/">Logout</a></li>
           </ul>
         </div><!--/.nav-collapse -->
@@ -58,21 +56,23 @@
     <div class="container">
 
 
-      <div class="row">
-        <div class="col-md-12">parte 1</div>
+      <div class="row extra-margin">
+        <div class="col-md-12">
+          <h2><span class="glyphicon glyphicon-asterisk"></span> Is the <strong>Synset</strong> a subtype of the <strong>Semantic Type</strong>?</h2>
+        </div>
       </div>
       <div class="row">
         <div class="col-md-6">
           <div class="panel panel-warning">
             <div class="panel-heading">
-              <h3 class="panel-title">Wordnet Synset</h3>
+              <h3 class="panel-title"><strong>Wordnet Synset</strong></h3>
             </div>
             <div class="panel-body">
               <dl class="dl-horizontal">
                 <dt>Words:</dt>
-                <dd>Exmaple, try-out, sample</dd>
+                <dd>Procurement, Procrurance, Procural</dd>
                 <dt>Gloss:</dt>
-                <dd>A demonstrative of something that must be taken in account while the evaluation is conducted.</dd>
+                <dd>the act of getting possession of something; "he was responsible for the procurement of materials and supplies".</dd>
               </dl>
             </div>
           </div>
@@ -80,15 +80,25 @@
         <div class="col-md-6">
           <div class="panel panel-success">
             <div class="panel-heading">
-              <h3 class="panel-title">Semantic Type</h3>
+              <h3 class="panel-title"><strong>Semantic Type</strong></h3>
             </div>
             <div class="panel-body">
-              Panel content
+              <dl class="dl-horizontal">
+                <dt>Name:</dt>
+                <dd>Animate</dd>
+                <dt>Definition:</dt>
+                <dd>Lorem ipsum dolor sit amet.</dd>
+                <hr/>
+                <dt>Name:</dt>
+                <dd>Animate</dd>
+                <dt>Definition:</dt>
+                <dd>Lorem ipsum dolor sit amet.</dd>
+              </dl>
             </div>
           </div>
         </div>
       </div>
-      <div class="row text-center">
+      <div class="row text-center extra-margin">
         <div class="col-md-12">
             <button type="button" class="btn btn-danger">
               <span class="glyphicon glyphicon-remove"></span> Wrong
@@ -100,8 +110,9 @@
       </div>
       <div class="row">
         <div class="col-md-12">
+          <p class="text-center text-muted"><small>Evaluations completed <span class="text-success"><strong>40%</strong></span> (325 of 1000)</small></p>
           <div class="progress">
-            <div class="progress-bar progress-bar-success" style="width: 40%"></div>
+            <div class="progress-bar progress-bar-info" style="width: 40%"></div>
           </div>
         </div>
       </div>
