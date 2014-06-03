f98d1af55a155923839f1b498bf066338b1aa069
felipeleao <felipe@fleao.com.br>
2014-06-03 00:47:09 -0300

    Changed queries for loading a mapping

--- dynamic/load_mapping.php
+++ dynamic/load_mapping.php
@@ -12,12 +12,40 @@
     //======================================================================
     //Retrieve the next mapping that should be evaluated by the user.
     //======================================================================
-    $sql_synset = "select s.id as 'id', group_concat(w.word SEPARATOR ', ') as 'words', s.gloss as 'gloss' ".
-                    " from synsets s inner join words w on w.id_synset = s.id ".
-                    " where s.id = (SELECT (last_evaluated+1) from users u where u.id='".$_SESSION['usuarioID']."');";
+    // $sql_synset = "select s.id as 'id', group_concat(w.word SEPARATOR ', ') as 'words', s.gloss as 'gloss' ".
+    //                 " from synsets s inner join words w on w.id_synset = s.id ".
+    //                 " where s.id = (SELECT (last_evaluated+1) from users u where u.id='".$_SESSION['usuarioID']."');";
+
+    //Retrieves a synset not yet ealuated by any user
+    $sql_synset = "select ".
+	                  "s.id as 'id', ".
+	                  "group_concat(w.word SEPARATOR ', ') AS 'words', ".
+	                  "s.gloss AS 'gloss' ".
+                    "FROM synsets s ".
+                    "INNER JOIN words w ON w.id_synset = s.id ".
+                    "WHERE s.id NOT IN ".
+                        "(SELECT id_synset FROM evaluation) ".
+                    "GROUP BY s.id ".
+                    "LIMIT 1; ";
+
 
     $query_synset = mysql_query($sql_synset);
     $result_synset = mysql_fetch_assoc($query_synset);
+
+
+    //======================================================================
+    //count evaluated synsets
+    //======================================================================
+    $sql_total_evaluated = "select count(*) as 'total' from evaluation e  group by e.id_synset;";
+
+    $query_total_evaluated = mysql_query($sql_total_evaluated);
+    $result_total_evaluated = mysql_fetch_assoc($query_total_evaluated);
+
+    if($result_total_evaluated[total] == null){
+        $total_evaluated = 0;
+    }else{
+        $total_evaluated = $result_total_evaluated[total];
+    }
 
 
     //======================================================================
@@ -30,7 +58,7 @@
 
 
     //======================================================================
-    //Retrieve the total of mappings that must be conducted (simply the number of synsets)
+    //Retrieve the semantic types related to the synset
     //======================================================================
     $sql_semtypes = "select st.name as 'name', st.definition as 'definition' from semantic_types st ".
                     "inner join synset_has_semtype sst on st.id = sst.id_semtype ".
@@ -65,7 +93,7 @@
                         'gloss'=>$result_synset[gloss]
                     ),
             'semtypes'=>$semtypes_array,
-            'synsetsEvaluated'=>$result_synset[id]-1,
+            'synsetsEvaluated'=>$total_evaluated,
             'totalSynsets'=>$result_total[total],
             'noMoreEvaluations'=>$is_evaluation_over);
 
